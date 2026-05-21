f8e9c4e9f6abde95c1e9d9868ac891e19831a44d
DivyasankariK <divyasankari.k07@gmail.com>
2026-05-21 08:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pickle
+import numpy as np
 
+# Load model
+with open("salary_prediction_model(1).pkl", "rb") as file:
+    model = pickle.load(file)
+
+st.title("Salary Prediction App")
+
+# User input
+experience = st.number_input("Enter Years of Experience", min_value=0.0)
+
+# Prediction
+if st.button("Predict Salary"):
+    prediction = model.predict([[experience]])
+
+    st.success(f"Predicted Salary: {prediction[0]:,.2f}")
